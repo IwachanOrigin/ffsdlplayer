53ff644a0c400de1879c2e5090c9669109c5e3d3
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2024-10-18 02:06:27 +0900

    [_prototype/05/02] Remove join. Use detach.

--- _prototype/05_simple_player_example/02_multi_thread/main/main.cpp
+++ _prototype/05_simple_player_example/02_multi_thread/main/main.cpp
@@ -1,4 +1,3 @@
-
 #include <Windows.h>
 #include <cstdio>
 #include <cstdlib>
@@ -22,29 +21,30 @@ void displayFrame(AVFrame*, SDL_Rect*, SDL_Texture*, SDL_Renderer*, double);
 void playaudio(AVCodecContext*, AVPacket*, AVFrame*, SDL_AudioDeviceID);
 
 // グローバル変数としてウィンドウとレンダラーを保持
-SDL_Window *screen = NULL;
-SDL_Renderer *renderer = NULL;
-SDL_Texture *texture = NULL;
+SDL_Window* screen = NULL;
+SDL_Renderer* renderer = NULL;
+SDL_Texture* texture = NULL;
 SDL_AudioDeviceID auddev;
 
 // スレッド間のデータ共有のためのキューと同期用の変数
 std::queue<AVPacket*> packetQueue;
 std::queue<AVFrame*> frameQueue;
 std::mutex packetMutex, frameMutex;
-std::condition_variable packetCv, frameCv;
+std::condition_variable packetCv, frameCv, renderFinishedCv;
 bool readFinished = false;
 bool decodeFinished = false;
+bool renderFinished = false;
 
 void readThread(AVFormatContext* pFormatCtx)
 {
   AVPacket* packet = av_packet_alloc();
   while (av_read_frame(pFormatCtx, packet) >= 0)
-    {
-      std::unique_lock<std::mutex> lock(packetMutex);
-      packetQueue.push(packet);
-      packetCv.notify_one();  // デコードスレッドに通知
-      packet = av_packet_alloc();  // 新しいパケットを準備
-    }
+  {
+    std::unique_lock<std::mutex> lock(packetMutex);
+    packetQueue.push(packet);
+    packetCv.notify_one();  // デコードスレッドに通知
+    packet = av_packet_alloc();  // 新しいパケットを準備
+  }
   readFinished = true;
   packetCv.notify_all();  // デコードスレッドに終了を通知
 }
@@ -54,37 +54,37 @@ void decodeThread(AVCodecContext* vidCtx, AVCodecContext* audCtx, int vidId, int
   AVFrame* vframe = av_frame_alloc();
   AVFrame* aframe = av_frame_alloc();
   while (true)
+  {
+    std::unique_lock<std::mutex> lock(packetMutex);
+    packetCv.wait(lock, [] { return !packetQueue.empty() || readFinished; });
+
+    if (packetQueue.empty() && readFinished)
     {
-      std::unique_lock<std::mutex> lock(packetMutex);
-      packetCv.wait(lock, [] { return !packetQueue.empty() || readFinished; });
-
-      if (packetQueue.empty() && readFinished)
-        {
-          break;  // 読み取りが終了していて、処理すべきパケットがない場合終了
-        }
-
-      AVPacket* packet = packetQueue.front();
-      packetQueue.pop();
-      lock.unlock();
-
-      if (packet->stream_index == vidId)
-        {
-          if (avcodec_send_packet(vidCtx, packet) == 0)
-            {
-              while (avcodec_receive_frame(vidCtx, vframe) == 0)
-                {
-                  std::unique_lock<std::mutex> frameLock(frameMutex);
-                  frameQueue.push(av_frame_clone(vframe));
-                  frameCv.notify_one();  // レンダリングスレッドに通知
-                }
-            }
-        }
-      else if (packet->stream_index == audId)
-        {
-          //playaudio(audCtx, packet, aframe, auddev);
-        }
-      av_packet_unref(packet);
+      break;  // 読み取りが終了していて、処理すべきパケットがない場合終了
     }
+
+    AVPacket* packet = packetQueue.front();
+    packetQueue.pop();
+    lock.unlock();
+
+    if (packet->stream_index == vidId)
+    {
+      if (avcodec_send_packet(vidCtx, packet) == 0)
+      {
+        while (avcodec_receive_frame(vidCtx, vframe) == 0)
+        {
+          std::unique_lock<std::mutex> frameLock(frameMutex);
+          frameQueue.push(av_frame_clone(vframe));
+          frameCv.notify_one();  // レンダリングスレッドに通知
+        }
+      }
+    }
+    else if (packet->stream_index == audId)
+    {
+      //playaudio(audCtx, packet, aframe, auddev);
+    }
+    av_packet_unref(packet);
+  }
   decodeFinished = true;
   frameCv.notify_all();  // レンダリングスレッドに終了を通知
   av_frame_free(&vframe);
@@ -94,116 +94,118 @@ void decodeThread(AVCodecContext* vidCtx, AVCodecContext* audCtx, int vidId, int
 void renderThread(SDL_Rect* rect, double fpsrendering)
 {
   while (true)
+  {
+    std::unique_lock<std::mutex> lock(frameMutex);
+    frameCv.wait(lock, [] { return !frameQueue.empty() || decodeFinished; });
+
+    if (frameQueue.empty() && decodeFinished)
     {
-      std::unique_lock<std::mutex> lock(frameMutex);
-      frameCv.wait(lock, [] { return !frameQueue.empty() || decodeFinished; });
-
-      if (frameQueue.empty() && decodeFinished)
-        {
-          break;  // デコードが終了していて、処理すべきフレームがない場合終了
-        }
-
-      AVFrame* frame = frameQueue.front();
-      frameQueue.pop();
-      lock.unlock();
-
-      displayFrame(frame, rect, texture, renderer, fpsrendering);
-      av_frame_free(&frame);
+      break;  // デコードが終了していて、処理すべきフレームがない場合終了
     }
+
+    AVFrame* frame = frameQueue.front();
+    frameQueue.pop();
+    lock.unlock();
+
+    displayFrame(frame, rect, texture, renderer, fpsrendering);
+    av_frame_free(&frame);
+  }
+  renderFinished = true;
+  renderFinishedCv.notify_one();
 }
 
 void play_video(const char* filename)
 {
-  AVFormatContext *pFormatCtx = nullptr;
+  AVFormatContext* pFormatCtx = nullptr;
   int vidId = -1, audId = -1;
   double fpsrendering = 0.0;
-  AVCodecContext *vidCtx, *audCtx;
-  AVCodecParameters *vidpar, *audpar;
+  AVCodecContext* vidCtx, * audCtx;
+  AVCodecParameters* vidpar, * audpar;
   SDL_Rect rect;
   SDL_AudioSpec want, have;
 
   pFormatCtx = avformat_alloc_context();
   char bufmsg[1024]{};
   if (avformat_open_input(&pFormatCtx, filename, NULL, NULL) < 0)
-    {
-      sprintf(bufmsg, "Cannot open %s", filename);
-      perror(bufmsg);
-      exit(EXIT_FAILURE);
-    }
+  {
+    sprintf(bufmsg, "Cannot open %s", filename);
+    perror(bufmsg);
+    exit(EXIT_FAILURE);
+  }
   if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
-    {
-      perror("Cannot find stream info. Quitting.");
-      exit(EXIT_FAILURE);
-    }
+  {
+    perror("Cannot find stream info. Quitting.");
+    exit(EXIT_FAILURE);
+  }
 
   for (int i = 0; i < pFormatCtx->nb_streams; i++)
+  {
+    AVCodecParameters* localparam = pFormatCtx->streams[i]->codecpar;
+    if (localparam->codec_type == AVMEDIA_TYPE_VIDEO)
     {
-      AVCodecParameters *localparam = pFormatCtx->streams[i]->codecpar;
-      if (localparam->codec_type == AVMEDIA_TYPE_VIDEO)
-        {
-          const AVCodec* vidCodec = avcodec_find_decoder(localparam->codec_id);
-          vidpar = localparam;
-          vidId = i;
-          AVRational rational = pFormatCtx->streams[i]->avg_frame_rate;
-          fpsrendering = 1.0 / ((double)rational.num / (double)(rational.den));
-          vidCtx = avcodec_alloc_context3(vidCodec);
-          if (avcodec_parameters_to_context(vidCtx, vidpar) < 0)
-            {
-              perror("vidCtx");
-              exit(EXIT_FAILURE);
-            }
+      const AVCodec* vidCodec = avcodec_find_decoder(localparam->codec_id);
+      vidpar = localparam;
+      vidId = i;
+      AVRational rational = pFormatCtx->streams[i]->avg_frame_rate;
+      fpsrendering = 1.0 / ((double)rational.num / (double)(rational.den));
+      vidCtx = avcodec_alloc_context3(vidCodec);
+      if (avcodec_parameters_to_context(vidCtx, vidpar) < 0)
+      {
+        perror("vidCtx");
+        exit(EXIT_FAILURE);
+      }
 
-          if (avcodec_open2(vidCtx, vidCodec, NULL) < 0)
-            {
-              perror("vidCtx");
-              exit(EXIT_FAILURE);
-            }
-        }
-      else if (localparam->codec_type == AVMEDIA_TYPE_AUDIO)
-        {
-          const AVCodec* audCodec = avcodec_find_decoder(localparam->codec_id);
-          audpar = localparam;
-          audId = i;
-          audCtx = avcodec_alloc_context3(audCodec);
-          if (avcodec_parameters_to_context(audCtx, audpar) < 0)
-            {
-              perror("audCtx");
-              exit(EXIT_FAILURE);
-            }
-
-          if (avcodec_open2(audCtx, audCodec, NULL) < 0)
-            {
-              perror("audCtx");
-              exit(EXIT_FAILURE);
-            }
-        }
+      if (avcodec_open2(vidCtx, vidCodec, NULL) < 0)
+      {
+        perror("vidCtx");
+        exit(EXIT_FAILURE);
+      }
     }
+    else if (localparam->codec_type == AVMEDIA_TYPE_AUDIO)
+    {
+      const AVCodec* audCodec = avcodec_find_decoder(localparam->codec_id);
+      audpar = localparam;
+      audId = i;
+      audCtx = avcodec_alloc_context3(audCodec);
+      if (avcodec_parameters_to_context(audCtx, audpar) < 0)
+      {
+        perror("audCtx");
+        exit(EXIT_FAILURE);
+      }
+
+      if (avcodec_open2(audCtx, audCodec, NULL) < 0)
+      {
+        perror("audCtx");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
 
   // ウィンドウとレンダラーを初期化（最初の一回だけ）
   if (screen == NULL)
+  {
+    screen = SDL_CreateWindow("Fplay", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+      vidpar->width, vidpar->height, SDL_WINDOW_OPENGL);
+    if (!screen)
     {
-      screen = SDL_CreateWindow("Fplay", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                                vidpar->width, vidpar->height, SDL_WINDOW_OPENGL);
-      if (!screen)
-        {
-          perror("screen");
-          exit(EXIT_FAILURE);
-        }
-      renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
-      if (!renderer)
-        {
-          perror("renderer");
-          exit(EXIT_FAILURE);
-        }
+      perror("screen");
+      exit(EXIT_FAILURE);
     }
+    renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+      perror("renderer");
+      exit(EXIT_FAILURE);
+    }
+  }
 
   // テクスチャの更新
   if (texture != NULL) {
     SDL_DestroyTexture(texture);
   }
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_IYUV,
-                              SDL_TEXTUREACCESS_STREAMING | SDL_TEXTUREACCESS_TARGET,
-                              vidpar->width, vidpar->height);
+    SDL_TEXTUREACCESS_STREAMING | SDL_TEXTUREACCESS_TARGET,
+    vidpar->width, vidpar->height);
   if (!texture) {
     perror("texture");
     exit(EXIT_FAILURE);
@@ -221,20 +223,25 @@ void play_video(const char* filename)
   auddev = SDL_OpenAudioDevice(NULL, 0, &want, &have, 0);
   SDL_PauseAudioDevice(auddev, 0);
   if (!auddev)
-    {
-      perror("auddev");
-      exit(EXIT_FAILURE);
-    }
+  {
+    perror("auddev");
+    exit(EXIT_FAILURE);
+  }
 
   // スレッドの開始
   std::thread reader(readThread, pFormatCtx);
   std::thread decoder(decodeThread, vidCtx, audCtx, vidId, audId);
   std::thread rendererThread(renderThread, &rect, fpsrendering);
 
-  reader.join();
-  decoder.join();
-  rendererThread.join();
+  reader.detach();
+  decoder.detach();
+  rendererThread.detach();
 
+  // 全てのスレッドが終了するのを待機
+  std::unique_lock<std::mutex> lock(frameMutex);
+  renderFinishedCv.wait(lock, [] { return renderFinished; });
+
+  // 後処理
   SDL_CloseAudioDevice(auddev);
   avcodec_free_context(&vidCtx);
   avcodec_free_context(&audCtx);
@@ -245,29 +252,33 @@ void play_video(const char* filename)
 void displayFrame(AVFrame* frame, SDL_Rect* rect, SDL_Texture* texture, SDL_Renderer* renderer, double fpsrendering)
 {
   SDL_UpdateYUVTexture(texture, rect,
-                       frame->data[0], frame->linesize[0],
-                       frame->data[1], frame->linesize[1],
-                       frame->data[2], frame->linesize[2]);
+    frame->data[0], frame->linesize[0],
+    frame->data[1], frame->linesize[1],
+    frame->data[2], frame->linesize[2]);
   SDL_RenderClear(renderer);
   SDL_RenderCopy(renderer, texture, NULL, rect);
   SDL_RenderPresent(renderer);
   SDL_Delay(static_cast<Uint32>(fpsrendering * 1000));
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   if (argc < 2)
-    {
-      printf("usage: %s <filename1> <filename2> ...\n", argv[0]);
-      exit(EXIT_FAILURE);
-    }
+  {
+    printf("usage: %s <filename1> <filename2> ...\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
   SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 
   for (int i = 1; i < argc; ++i)
-    {
-      play_video(argv[i]);
-    }
+  {
+    play_video(argv[i]);
+    // 各スレッドの完了フラグをリセット
+    readFinished = false;
+    decodeFinished = false;
+    renderFinished = false;
+  }
 
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(screen);
